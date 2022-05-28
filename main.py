a65cd65a9a1affde86013d54535f950669944b0d
EzenwakaAdachukwu <105318332+EzenwakaAdachukwu@users.noreply.github.com>
2022-05-28 02:16:32 -0700

    zuri commit

---
+++ main.py
@@ -0,0 +1,39 @@
+class Student():
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+         
+        self.name = str(name) 
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)   
+
+    def change_name(self, change_name):
+        self.change_name = change_name
+        # print (self.name)
+        print("your name is" , change_name)
+
+    def change_age(self, change_age):
+        self.change_age = change_age
+        # print (self.age)
+        print("your age is" , change_age)
+  
+    def add_track(self, add_tracks):
+        self.add_track = add_tracks
+        # print (self.track)
+        print("your track is" , add_tracks)
+    
+    def get_score(self):
+        # return (self.score)
+        print( self.score)   
+    
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+
+
+
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
